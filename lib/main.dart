415437d4c5dceb87e544dcae6a2cb4c3c389037a
Bright A. <bright.amoateng150@gmail.com>
2024-06-28 22:15:58 -0400

    add: appbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,62 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          leading: const Icon(
+            color: Color(0xffa7a7a7),
+            Icons.menu,
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 12),
+              child: Icon(
+                color: Color(0xffa7a7a7),
+                Icons.dark_mode_outlined,
+              ),
+            ),
+          ],
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: const Color(0xff009788),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: const Text(
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xffffffff),
+                    height: 1.1,
+                  ),
+                  "Calculator",
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.transparent,
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: const Text(
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xffc1c1c1),
+                    height: 1.1,
+                  ),
+                  "Converter",
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: const Center(
           child: Text('Hello World!'),
         ),
       ),
