4ece8b4efbfafe2548b8eba84b057bcaa9bf1769
Bright A. <bright.amoateng150@gmail.com>
2024-06-30 19:43:30 -0400

    update: button pad to use icons and different colors

--- lib/widgets/button_pad.dart
+++ lib/widgets/button_pad.dart
@@ -10,58 +10,186 @@ class ButtonPad extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            _buildButton("AC", color: const Color(0xff403e4c)),
-            _buildButton("<", color: const Color(0xff403e4c)),
-            _buildButton("%", color: const Color(0xff009788)),
-            _buildButton("/", color: const Color(0xff009788)),
+            _buildButton(
+              text: "AC",
+              onSurfaceColor: const Color(0xff403e4c),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.white,
+              // surfaceColor: const Color(0xff6c807e), // dark
+            ),
+            _buildButton(
+              icon: const Icon(Icons.backspace_outlined),
+              onSurfaceColor: const Color(0xff403e4c),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.white,
+              // surfaceColor: const Color(0xff6c807e), // dark
+            ),
+            _buildButton(
+              icon: const Icon(Icons.percent),
+              onSurfaceColor: const Color(0xff009788),
+              surfaceColor: Colors.white,
+              // surfaceColor: Colors.white, // dark
+            ),
+            _buildButton(
+              text: "/",
+              onSurfaceColor: const Color(0xff009788),
+              surfaceColor: Colors.white,
+              // surfaceColor: Colors.white, // dark
+            ),
           ],
         ),
         const SizedBox(height: 8),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            _buildButton("7", color: const Color(0xff808080)),
-            _buildButton("8", color: const Color(0xff808080)),
-            _buildButton("9", color: const Color(0xff808080)),
-            _buildButton("x", color: const Color(0xff009788)),
+            _buildButton(
+              text: "7",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "8",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "9",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              icon: const Icon(Icons.close),
+              onSurfaceColor: const Color(0xff009788),
+              surfaceColor: Colors.white,
+              // surfaceColor: Colors.white, // dark
+            ),
           ],
         ),
         const SizedBox(height: 8),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            _buildButton("4", color: const Color(0xff808080)),
-            _buildButton("5", color: const Color(0xff808080)),
-            _buildButton("6", color: const Color(0xff808080)),
-            _buildButton("-", color: const Color(0xff009788)),
+            _buildButton(
+              text: "4",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "5",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "6",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              icon: const Icon(Icons.remove),
+              onSurfaceColor: const Color(0xff009788),
+              surfaceColor: Colors.white,
+              // surfaceColor: Colors.white, // dark
+            ),
           ],
         ),
         const SizedBox(height: 8),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            _buildButton("1", color: const Color(0xff808080)),
-            _buildButton("2", color: const Color(0xff808080)),
-            _buildButton("3", color: const Color(0xff808080)),
-            _buildButton("+", color: const Color(0xff009788)),
+            _buildButton(
+              text: "1",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "2",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "3",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              icon: const Icon(Icons.add),
+              onSurfaceColor: const Color(0xff009788),
+              surfaceColor: Colors.white,
+              // surfaceColor: Colors.white, // dark
+            ),
           ],
         ),
         const SizedBox(height: 8),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            _buildButton("+-", color: const Color(0xff403e4c)),
-            _buildButton("0", color: const Color(0xff808080)),
-            _buildButton(".", color: const Color(0xff403e4c)),
-            _buildButton("=", color: const Color(0xff009788)),
+            _buildButton(
+              text: "+-",
+              onSurfaceColor: const Color(0xff403e4c),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "0",
+              onSurfaceColor: const Color(0xff808080),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: ".",
+              onSurfaceColor: const Color(0xff403e4c),
+              // onSurfaceColor: Colors.white, // dark
+              surfaceColor: Colors.transparent,
+            ),
+            _buildButton(
+              text: "=",
+              onSurfaceColor: const Color(0xff009788),
+              surfaceColor: Colors.white,
+              // onSurfaceColor: Colors.white, // dark
+            ),
           ],
         ),
       ],
     );
   }
 
-  Widget _buildButton(String text, {required Color color}) {
+  Widget _buildButton({
+    String? text,
+    Icon? icon,
+    // void Function()? onTap,
+    required Color onSurfaceColor,
+    required Color surfaceColor,
+  }) {
+    Widget? label;
+    if (text != null) {
+      label = Text(
+        style: TextStyle(
+          fontSize: 22,
+          height: 1.1,
+          color: onSurfaceColor,
+        ),
+        text,
+      );
+    } else if (icon != null) {
+      label = IconTheme(
+        data: IconThemeData(
+          color: onSurfaceColor,
+        ),
+        child: icon,
+      );
+    }
+
     return Material(
+      color: surfaceColor,
       borderRadius: BorderRadius.circular(100),
       child: InkWell(
         borderRadius: BorderRadius.circular(100),
@@ -69,16 +197,8 @@ class ButtonPad extends StatelessWidget {
         child: SizedBox(
           width: 64,
           height: 64,
-          // padding: const EdgeInsets.all(16),
           child: Center(
-            child: Text(
-              style: TextStyle(
-                fontSize: 22,
-                height: 1.1,
-                color: color,
-              ),
-              text,
-            ),
+            child: label,
           ),
         ),
       ),
